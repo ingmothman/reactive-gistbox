001ffc26a629dd4db5f88619bb741b1f3b16290e
Mohammed Othman <osmancode@gmail.com>
2017-08-07 17:56:41 +0200

    ejected create-react-app

--- src/index.js
+++ src/index.js
@@ -9,12 +9,27 @@ import App from './components/App'
 
 import {Provider} from 'react-redux';
 import store from './store/index';
+import {AppContainer} from 'react-hot-loader';
 
 import registerServiceWorker from './registerServiceWorker';
 
 render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
+    <AppContainer>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </AppContainer>
     , document.getElementById('root'));
 registerServiceWorker();
+
+if (module.hot) {
+    module.hot.accept('./components/App', () => {
+        const NextApp = require('./components/App').default; // eslint-disable-line global-require
+        render(
+            <AppContainer>
+                <NextApp />
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
+}
