4a355d84937941f217d4ced55c1821a4f73c2dac
Mohammed Othman <osmancode@gmail.com>
2017-08-14 15:05:25 +0200

    refactored store code.

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,29 @@
-import {createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {createLogger} from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
-const enhancer = applyMiddleware(
-    // logger,
+const middlewares = [
     thunk,
-);
+];
 
-const reduxDevToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// Logger must be the last middleware in chain
+if (process.env.NODE_ENV !== 'production') {
+    const logger = createLogger({
+        collapsed: true,
+    });
+    middlewares.push(logger);
+}
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
 export default createStore(
     reducers,
-    reduxDevToolsExtension,
     enhancer
 );
+
+
